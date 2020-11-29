d6fe6cdbd79eb991babdc92ff41b441c3d63d07a
rojae <Jae.Thon96@gmail.com>
2020-11-29 21:09:16 +0900

    ADD) edit entity with jpa log

--- src/main/java/org/rojae/examples/Account.java
+++ src/main/java/org/rojae/examples/Account.java
@@ -4,28 +4,30 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
-@Getter
-@Setter
 @Builder
 public class Account {
 
     @Id @GeneratedValue
     private Long id;
 
-    @Column
+    @Column(nullable = false, unique = true)
     private String username;
 
     @Column
     private String password;
 
+    @Temporal(TemporalType.TIMESTAMP)
+    private final Date created = new Date();
+
+    @Transient
+    private String po;
+
     @Override
     public String toString(){
-        return "id: "+id+", username :"+username+", password: "+ password;
+        return "id: "+id+", username :"+username+", password: "+ password + ", createdDate: "+created;
     }
 }
